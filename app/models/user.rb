0357fedd5c3806f108e19b88b0d5efd19f37762f
Ivan Diaz <ivanid22@gmail.com>
2020-06-17 19:57:24 -0300

    Add base styles and User controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  VALID_USERNAME_REGEX = /^[A-z]+$/
+  VALID_FULL_NAME_REGEX = /^([A-z]+[A-z ]+)+$/
+
   has_many :game_reviews, class_name: "GameReview", foreign_key: "AuthorId"
 
   has_many :outgoing_followings, class_name: "Following", foreign_key: "FollowerId"
@@ -6,4 +9,10 @@ class User < ApplicationRecord
 
   has_many :followers, class_name: "User", through: :incoming_followings, foreign_key: "FollowedId"
   has_many :followed, class_name: "User", through: :outgoing_followings, foreign_key: "FollowerId"
+
+  validates :Username, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 15} #format: { with: VALID_USERNAME_REGEX }
+  validates :FullName, presence: true, length: {maximum: 50}
+  
+  
+  
 end
