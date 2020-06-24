a1cf0df70e9f51bdddd8767be73307cd2443f704
Ivan Diaz <ivanid22@gmail.com>
2020-06-24 13:16:14 -0300

    Fix user_options button and who_to_follow_list, moved logic to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -18,4 +18,13 @@ module UsersHelper
   def user_cover_picture_url(user)
     user.cover_picture.attached? ? url_for(user.cover_picture) : asset_url('default_cover.jpg')
   end
+
+  def user_options_button(user)
+    show_b = logged_in_user && (user.id != logged_in_user.id)
+    return '<a class=user-page__user-action-button href=# ><i class=fas fa-list fa-xl></i></a>'.html_safe if show_b
+  end
+
+  def followable_user_list
+    session[:logged_in_user_id] ? User.find(session[:logged_in_user_id]).followable : User.all
+  end
 end
