a4177ab6453eb422fba3578cd64ede13e855af6e
Ivan Diaz <ivanid22@gmail.com>
2020-06-24 12:37:14 -0300

    Fix like button logic to be on helper

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -2,4 +2,9 @@ module LikesHelper
   def like_path(game_review)
     "/like/#{game_review.id}"
   end
+
+  def like_button(review)
+    not_liked = review.likes.all.includes(:user).where(user: logged_in_user).empty?
+    return "<a href=#{like_path(review)} class=like-button><i class=fas fa-thumbs-up></i></a>".html_safe if not_liked
+  end
 end
